bce62ae91a5b289e8804e4851bc3793a8022741a
Marvin Fuchs <info@marvin-fuchs.de>
2025-06-02 13:06:10 +0200

    stuff

--- calculator.asm
+++ calculator.asm
@@ -1,8 +1,3 @@
-init_calculator:
-	display	EQU	0x48
-	MOV	display,0
-	RET
-
 ;--------------------------------------------------------------------------
 ; Reads numbers until an action key is pressed
 ; Outputs:
@@ -11,11 +6,11 @@ init_calculator:
 ;--------------------------------------------------------------------------
 read_number:
 	num	EQU	0x30
-	MOV	num,	0
+	MOV	num,	#00h
 calculator_read_number_loop:
 	CALL	read_keypad
 	MOV	R0,	A
-	ANL	A,	#0F0H
+	ANL	A,	#0F0h
 	JNZ	calculator_ret ; Actions are not 0 -> handle end of number reading
 	MOV	A,	num
 	MOV	B,	#10d
@@ -28,3 +23,56 @@ calculator_ret:
 	MOV	B,	R0
 	RET
 
+;--------------------------------------------------------------------------
+; Calculates the new truth given a number and operation
+; Inputs:
+;	A: The number
+;	B: The operation (see keypad)
+; Side effect:
+;	Outputs the calculation to the truth data
+;--------------------------------------------------------------------------
+calculate:
+	MOV	R0,	A
+	MOV	A,	B
+	SWAP	A
+	CJNE 	A, #1111b, check_plus
+	JMP 	op_equal
+check_plus:
+	CJNE	A, #0001b, check_minus
+	JMP 	op_add
+check_minus:
+	CJNE	A, #0010b, check_multiply
+	JMP	op_minus
+check_multiply:
+	CJNE	A, #0100b, op_divide
+	JMP	op_multiply
+
+op_equal:
+	MOV	truth,	R0
+	RET
+
+op_add:
+	MOV	A,	truth
+	ADD	A,	R0
+	MOV	A,	truth
+	RET
+
+op_minus:
+	MOV	A,	truth
+	SUBB	A,	R0
+	MOV	A,	truth
+	RET
+
+op_multiply:
+	MOV	A,	truth
+	MOV	B,	R0
+	MUL	AB
+	MOV	A,	truth
+	RET
+
+op_divide:
+	MOV	A,	truth
+	MOV	B,	R0
+	DIV	AB
+	MOV	A,	truth
+	RET
--- main.asm
+++ main.asm
@@ -1,19 +1,20 @@
 call 	init_keypad
-call	init_calculator
 call	clear_display
+
+truth	DATA	50h
+MOV	P3,	#00h
 
 main_loop:
 	CALL	read_number
 
-	;CALL	calculate
-	;CALL	write_number
+	CALL	calculate
 
-	CALL	display_number
+	MOV	P3,	truth
+
 	JMP	main_loop
-
-
 
 
 include 'keypad.asm'
 include 'calculator.asm'
 include	'display.asm'
+END
